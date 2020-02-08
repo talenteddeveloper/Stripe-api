070f5f1e45a65d8e151f7dcd89017eb09611d861
Talented Developer <40318152+talenteddeveloper@users.noreply.github.com>
2020-02-08 22:54:27 +0530

    Create WebhookTest.java

---
+++ WebhookTest.java
@@ -0,0 +1,27 @@
+
+
+import java.util.*;
+
+import com.stripe.*;
+import com.stripe.exception.StripeException;
+import com.stripe.model.WebhookEndpoint;
+
+
+public class WebhookTest {
+
+	public static void main(String[] args) throws StripeException {
+		
+    	Stripe.apiKey ="sk_test_BdavkaT4hXelQXTZa7NzhS4k";
+    	List<Object> events = new ArrayList<Object>();
+    	events.add("charge.succeeded");
+    	events.add("charge.failed");
+    	
+    	Map<String, Object> param = new HashMap<String, Object>();
+    	param.put("enabled_events",events);
+    	param.put("url", "https://webhook.site/6771f998-390c-4963-a821-e40289dfcac0");
+    	
+    	WebhookEndpoint weEndpoint = WebhookEndpoint.create(param);
+    	System.out.println(weEndpoint);
+	}
+
+}
